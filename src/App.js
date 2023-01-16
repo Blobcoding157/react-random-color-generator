793d12ba4ae96fc64a3e68490f67fd9b9c5d209d
Blobcoding157 <patrik.nuernberger@gmail.com>
2023-01-16 14:14:37 +0100

    Add Button and RandomHex Generator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
-import logo from './logo.svg';
+import somewhatUsefulFunctions from './somewhatUsefulFunctions';
 
 function App() {
-  return <div className="App"></div>;
+  const { color, generateColor } = somewhatUsefulFunctions();
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#' + color,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <button
+        style={{
+          padding: '40px',
+          borderRadius: '10px',
+          backgroundImage: 'linear-gradient(to top, #a8edea 0%, #fed6e3 100%)',
+          fontSize: 'larger',
+        }}
+        onClick={generateColor}
+      >
+        Generate
+      </button>
+    </div>
+  );
 }
 
 export default App;
