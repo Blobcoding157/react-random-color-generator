b56e6fe19f4afe86e237d0bb8f34d887a6ed699d
Blobcoding157 <patrik.nuernberger@gmail.com>
2023-01-16 16:43:41 +0100

    Fix for Preflight

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,15 +1,33 @@
-const reportWebVitals = async (onPerfEntry) => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    await import('web-vitals').then(
-      ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
         getCLS(onPerfEntry);
         getFID(onPerfEntry);
         getFCP(onPerfEntry);
         getLCP(onPerfEntry);
         getTTFB(onPerfEntry);
-      },
-    );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 };
 
 export default reportWebVitals;
+
+// const reportWebVitals = async (onPerfEntry) => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     await import('web-vitals').then(
+//       ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//         getCLS(onPerfEntry);
+//         getFID(onPerfEntry);
+//         getFCP(onPerfEntry);
+//         getLCP(onPerfEntry);
+//         getTTFB(onPerfEntry);
+//       },
+//     );
+//   }
+// };
+
+// export default reportWebVitals;
